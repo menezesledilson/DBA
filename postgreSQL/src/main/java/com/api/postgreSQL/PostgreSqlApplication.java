ee7635c32986077f6e6f8a1147add4589bb9d131
menezesledilson <ledilsonmenezes@gmail.com>
2024-05-28 17:15:59 -0300

    api de backup

---
+++ postgreSQL/src/main/java/com/api/postgreSQL/PostgreSqlApplication.java
@@ -0,0 +1,15 @@
+package com.api.postgreSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreSqlApplication.class, args);
+
+
+	}
+
+}
